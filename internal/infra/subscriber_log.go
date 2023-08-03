5a09da1ecd39725a93dfdb22c9fc9870d3183be1
Adrien Aury <44274230+adrienaury@users.noreply.github.com>
2023-08-03 11:53:56 +0000

    fix: log message

--- internal/infra/subscriber_log.go
+++ internal/infra/subscriber_log.go
@@ -9,5 +9,5 @@ func (sl SubscriberLogger) NewField(fieldname string) {
 }
 
 func (sl SubscriberLogger) FirstNonMaskedValue(fieldname string, value any) {
-	log.Warn().Str("name", fieldname).Msg("field is not completely masked")
+	log.Info().Str("name", fieldname).Msg("unmasked value detected")
 }
