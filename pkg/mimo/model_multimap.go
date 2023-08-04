4750b201ec566b95001434b8fe3a8d5ca52046e3
Adrien Aury <44274230+adrienaury@users.noreply.github.com>
2023-08-04 15:44:55 +0200

    style: lint

--- pkg/mimo/model_multimap.go
+++ pkg/mimo/model_multimap.go
@@ -2,7 +2,7 @@ package mimo
 
 type Multimap map[string]map[string]int
 
-// Add a key/value pair to the multimap
+// Add a key/value pair to the multimap.
 func (m Multimap) Add(key string, value string) {
 	set, exists := m[key]
 	if !exists {
@@ -14,7 +14,7 @@ func (m Multimap) Add(key string, value string) {
 	m[key] = set
 }
 
-// Count the number of values associated to key
+// Count the number of values associated to key.
 func (m Multimap) Count(key string) int {
 	set, exists := m[key]
 	if !exists {
@@ -24,7 +24,7 @@ func (m Multimap) Count(key string) int {
 	return len(set)
 }
 
-// Rate return the percentage of keys that have a count of 1
+// Rate return the percentage of keys that have a count of 1.
 func (m Multimap) Rate() float64 {
 	cnt := 0
 
